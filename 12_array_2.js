f01904b5455e6b500f93e69fb793c0683bffb4e8
ANISH CHAKRABORTY <anishchakraborty35@gmail.com>
2026-08-14 01:11:52 +0530

    array

---
+++ 12_array_2.js
@@ -0,0 +1,36 @@
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push (dc_heros)
+
+
+// console.log(marvel_heros)
+// console.log(marvel_heros[3][1])
+
+// const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros)
+
+
+const all_new_heros = [...marvel_heros,...dc_heros]// spread opertor 
+
+// console.log(all_new_heros)
+
+
+const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+
+const real_another_array= another_array.flat(Infinity)
+
+
+// console.log(real_another_array)
+
+
+console.log(Array.isArray("anish"))
+console.log(Array.from("anish")) //  string, object, numbers to array
+console.log(Array.from({name: "anish"}))// interesting case
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3))
