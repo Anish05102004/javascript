0e464cd94223b92e550420828a60681b8a554677
ANISH CHAKRABORTY <anishchakraborty35@gmail.com>
2026-08-16 00:23:20 +0530

    json

---
+++ 15_objects_3.js
@@ -0,0 +1,26 @@
+const course = {
+    coursename:"javascript",
+    price:"999",
+    courseInstructor : "abc"
+}
+
+
+// course.courseInstructor
+
+const{courseInstructor:instructor}= course // destructuring
+
+console.log(instructor)
+
+
+// {
+//     "name":"anish",
+//     "coursename":"js",
+//     "price":"999"
+// }
+
+
+// [
+// {},
+// {},
+// {}
+// ]
