c28ea12a4b64d6872b48e0c4c475e47635b894dc
ANISH CHAKRABORTY <anishchakraborty35@gmail.com>
2026-08-17 23:45:22 +0530

    loop, break, continue

---
+++ iteration.js/one.js
@@ -0,0 +1,49 @@
+// for loop
+
+for (let index = 0; index <= 10; index++) {
+    const element = index;
+    if(element == 5) {
+        // console.log("5 is good no")
+    }
+    // console.log(element)
+}
+
+
+for (let i = 0; i <= 10; i++) {
+    // console.log(`outer loop value: ${i}`)
+    for (let j = 0; j <= 10; j++) {
+        // console.log(`inner loop value: ${j} and inner loop ${i}`)
+        // console.log(i + ' * ' + j + ' = ' + i * j) // print 0 - 10 multiplication table
+    }  
+    }
+
+
+    let myarray = ["flash", "batman", "superman"]
+    // console.log(myarray.length);
+    
+    for (let index = 0; index < myarray.length; index++) {
+        const element = myarray[index];
+        // console.log(element);
+    }
+
+
+    // break 
+
+    for (let index = 0; index <=20 ; index++) {
+        if(index == 5) {
+            // console.log(`5 is found `);
+            break     
+        }
+        // console.log(`value of i is ${index}`);
+    }
+
+
+    // continue
+    
+        for (let index = 0; index <=20 ; index++) {
+        if(index == 5) {
+            // console.log(`5 is found `);
+            continue    
+        }
+        // console.log(`value of i is ${index}`);
+    }
